ff1669e81e901706c321df1940ee0dcf5e4500f8
om fale <omfale1001@gmail.com>
2025-03-16 12:06:59 -0400

    Done Binary Search 1

---
+++ 2dMatrix.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int low=0;
+        int high= (m*n) - 1;
+
+        while(low <= high)
+        {
+            int mid = low + (high - low)/2;
+            
+            if(matrix[mid / n][mid % n] > target)
+            {
+                high = mid - 1;
+            }
+            else if(matrix[mid / n][mid % n] < target)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+    return false;
+
+    }
+}
